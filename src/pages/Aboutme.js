cb58def4df30be455db477de17c141c23578e033
Clayton Grubick <ccg0015@mix.wvu.edu>
2022-12-15 22:17:11 -0500

    refresh error bug fix #1

--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -14,7 +14,8 @@ function Aboutme() {
         <h2>About me</h2>
             <p> I am working on a double major from West Virginia University in both Computer Engineering, and Electrical Engineering, <br/> 
             and am receiving a minor in Computer Science. I have interest in computer hardware, specifically hardware design <br/>
-            and computer architecture. I am a West Virginian native and grew up in Weirton, WV. I am currently a senior and plan to graduate in December of 2023.<br/>
+            and computer architecture. I am currently a senior and plan to graduate in December of 2023. I have had a passion for computers ever since middle school.<br/>
+            In 8th grade I decided after learning what computer engineering was that I had to be one and I am happy to be able to begin to make that dream come true!<br/>
             You can also find my LinkedIn and Github in the footer for my resume and more in depth look at some of my projects!
 
             <br/>
